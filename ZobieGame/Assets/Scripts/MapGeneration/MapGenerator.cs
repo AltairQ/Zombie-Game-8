330de9480c4976c70c443dcbcd08849b1ba458f7
Bartosz Rudzki (Home) <bartosz.rudzki@baadgames.com>
2017-12-17 02:01:08 +0100

    MapGenerator scene and simple scripts created.

---
+++ ZobieGame/Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour
+{
+    private GameObject _lastHouse = null;
+    private int num = 0;
+    public void GenerateHouse(float width, float height, float depth)
+    {
+        DestroyImmediate(_lastHouse);
+
+        _lastHouse = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        _lastHouse.name = "House" + num;
+        num++;
+
+        _lastHouse.transform.rotation = Quaternion.Euler(new Vector3(90, 0));
+        _lastHouse.transform.localScale = new Vector3(width, depth, 1);
+
+
+    }
+}
