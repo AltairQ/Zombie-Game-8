aeb24e7a10cf1eb1542d8a90e4270d8dbc08c342
Adameq7 <adamchabecki@gmail.com>
2017-12-28 04:34:39 +0100

    Ammunition system introduced, ammo pickups added.

---
+++ ZobieGame/Assets/Scripts/Gameplay/AmmoPickupScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickupScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * 60);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerScript>().Restock(0.25f);
+            Destroy(this.gameObject);
+        }
+    }
+}
