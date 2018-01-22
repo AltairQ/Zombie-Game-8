79c8aa1159354c86e7fd9dc0311d0b9ae78a6842
Adameq7 <adamchabecki@gmail.com>
2018-01-22 15:49:27 +0100

    GameDirector init.

--- ZobieGame/Assets/Scripts/AI/GameDirector.cs
+++ ZobieGame/Assets/Scripts/AI/GameDirector.cs
@@ -4,6 +4,19 @@ using System.Collections.Generic;
 
 public class GameDirector{
 
+    System.Random rnd = new System.Random();
+
+    float health_base = 50;
+    float health_step = 5;
+    float speed_base = 2;
+    float speed_step = 0.1f;
+    float strength_base = 10;
+    float strength_step = 2;
+    float melee_range_base = 1.0f;
+    float melee_range_step = 0.1f;
+    float armor_base = 0;
+    float armor_step = 0.5f;
+
     // Bundle of info to be stored in the GD's database
     class ActorInfo
     {
@@ -64,14 +77,34 @@ public class GameDirector{
 
         _lastId++;
 
+        int[] stats = new int[5];
+        int[] stats_best = new int[5];
+
+        int best_level = 0;
+
+        if (_candidates.Count > 0)
+        {
+            stats_best[0] = (int)((_database[_candidates[0]].genes.G_health - health_base) / health_step);
+            stats_best[1] = (int)((_database[_candidates[0]].genes.G_melee_range - melee_range_base) / melee_range_step);
+            stats_best[2] = (int)((_database[_candidates[0]].genes.G_speed - speed_base) / speed_step);
+            stats_best[3] = (int)((_database[_candidates[0]].genes.G_strength - strength_base) / strength_step);
+            stats_best[4] = (int)((_database[_candidates[0]].genes.G_armor - armor_base) / armor_step);
+
+            best_level = _database[_candidates[0]].genes.level;
+        }
+
+        for (int i = 0; i < _tick_count - best_level; i++)
+            stats[rnd.Next(0, 4)] += 1;
+
         Genes newgenes = new Genes
         {
+            level = _tick_count,
             Id = _lastId,
-            G_health = 100.0f,
-            G_melee_range = 2.0f,
-            G_speed = 1.0f,
-            G_strength = 10.0f,
-            G_armor = 0.0f
+            G_health = health_base + (stats[0] + stats_best[0]) * health_step,
+            G_melee_range = melee_range_base + (stats[1] + stats_best[1]) * melee_range_step,
+            G_speed = speed_base + (stats[2] + stats_best[2]) * speed_step,
+            G_strength = strength_base + (stats[3] + stats_best[3]) * strength_step,
+            G_armor = armor_base + (stats[4] + stats_best[4]) * armor_step
         };
 
         Memes newmemes = new Memes
@@ -101,6 +134,7 @@ public class GameDirector{
         where T : IAIEnvActions, IAIEnvState
     {
         _tick_count++;
+
 
         warudo.SpawnEnemy(this.NewEnemy());
     }
--- ZobieGame/Assets/Scripts/AI/Genes.cs
+++ ZobieGame/Assets/Scripts/AI/Genes.cs
@@ -2,6 +2,8 @@
 // Bundle containing physical parameters and also ID
 public class Genes{
 
+    public int level;
+
     // Unique enemy ID (used by Game Director)
     public int Id;
 
@@ -42,5 +44,15 @@ public class Genes{
     public float GetPhysMeleeRange()
     {
         return G_melee_range;
+    }
+
+    public int Level()
+    {
+        return level;
+    }
+
+    override public string ToString()
+    {
+        return "level: " + level + " ID: " + Id + " health: " + G_health + " speed: " + G_speed + " strength: " + G_strength + " melee range: " + G_melee_range + " armor: " + G_armor;
     }
 }
