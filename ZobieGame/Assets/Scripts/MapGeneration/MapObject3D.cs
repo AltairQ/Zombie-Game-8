5ce91db4198aa028af86562150653778e71557b3
Bartosz Rudzki (Home) <bartosz.rudzki@baadgames.com>
2018-02-06 18:44:42 +0100

    More patterns/shapes added. Code cleaning.

--- ZobieGame/Assets/Scripts/MapGeneration/MapObject3D.cs
+++ ZobieGame/Assets/Scripts/MapGeneration/MapObject3D.cs
@@ -33,7 +33,7 @@ public class MapObject3D : MapObject
         GameObject go = Utils.TerrainObject(_name);
         for(int i=0; i<_partSettings.Length; i++)
         {
-            var partGO = MakePart2(i);
+            var partGO = MakePart(i);
             partGO.SetParent(go);
             //go.Combine(partGO);
             //partGO.Clear(GameObject.DestroyImmediate);
@@ -42,48 +42,6 @@ public class MapObject3D : MapObject
     }
 
     private GameObject MakePart(int k)
-    {
-        float partYOff = 0;
-        for(int i = 0; i<k; i++)
-        {
-            partYOff += _partHeights[i];
-        }
-
-        var setting = _partSettings[k];
-        float height = _partHeights[k];
-        var go = Utils.TerrainObject(setting.Name);
-        int maxN = setting.SegmentsNumber - 1;
-        float segmentHeight = height / maxN;
-        float maxScale = 0f;
-        for (int i=0; i<=maxN; i++)
-        {
-            float sizeScale = setting.Shape.Evaluate((float)i / maxN);
-            maxScale = Mathf.Max(sizeScale, maxScale);
-            var segment = MakeSegment(sizeScale, segmentHeight, segmentHeight * i + partYOff);
-            segment.SetMaterial(setting.Material);
-            segment.SetParent(go);
-        }
-
-        go.transform.position = Rect.Center(0f);
-        var box = go.AddComponent<BoxCollider>();
-        box.size = new Vector3(Rect.width * maxScale, height, Rect.height * maxScale);
-        box.center = new Vector3(0f, partYOff + height / 2, 0f);
-
-        return go;
-    }
-
-    private GameObject MakeSegment(float sizeScale, float height, float yOff)
-    {
-        var segment = Utils.TerrainObject(PrimitiveType.Cube, "Segment");
-        segment.GetComponent<BoxCollider>().enabled = false;
-        Vector2 rectSize = Rect.size * sizeScale;
-        segment.transform.localScale = new Vector3(rectSize.x, height, rectSize.y);
-        segment.transform.position = new Vector3(0f, yOff + height/2, 0f);
-
-        return segment;
-    }
-
-    private GameObject MakePart2(int k)
     {
         float partYOff = 0;
         for (int i = 0; i < k; i++)
@@ -97,11 +55,12 @@ public class MapObject3D : MapObject
         int maxN = setting.SegmentsNumber - 1;
         float segmentHeight = height / maxN;
         float maxScale = 0f;
-        var pattern = setting.GetPattern();
+        var pattern = setting.RandomPattern();
+        var shape = setting.RandomShape();
         SegmentMeshGenerator meshGen = new SegmentMeshGenerator(pattern);
         for (int i = 0; i <= maxN; i++)
         {
-            float sizeScale = setting.Shape.Evaluate((float)i / maxN);
+            float sizeScale = shape.Evaluate((float)i / maxN);
             maxScale = Mathf.Max(sizeScale, maxScale);
             meshGen.AddSegment(Rect.width * sizeScale, segmentHeight * i + partYOff);
         }
--- ZobieGame/Assets/Scripts/MapGeneration/Settings/Object3DSetting.cs
+++ ZobieGame/Assets/Scripts/MapGeneration/Settings/Object3DSetting.cs
@@ -5,21 +5,33 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "Object3DSetting", menuName = "Generator/Object3DSetting")]
 public class Object3DSetting : ScriptableObject
 {
+    [System.Serializable]
+    public class LPattern
+    {
+        public string Axiom;
+        public string FMove;
+        public int Iterations;
+    }
+
     public string Name;
     public int SegmentsNumber;
     public float MinHeight;
     public float MaxHeight;
 
-    public AnimationCurve Shape;
     public Material Material;
+    public AnimationCurve[] Shapes;
+    public LPattern[] Patterns;
 
-    public string Axiom;
-    public string FMove;
-    public int Iterations;
-
-    private List<Vector2> _pattern = new List<Vector2>();
-    public List<Vector2> GetPattern()
+    public AnimationCurve RandomShape()
     {
-        return PatternGenerator.GeneratePattern(Axiom, FMove, Iterations);
+        int idx = Random.Range(0, Shapes.Length);
+        return Shapes[idx];
+    }
+
+    public List<Vector2> RandomPattern()
+    {
+        int idx = Random.Range(0, Patterns.Length);
+        LPattern pattern = Patterns[idx];
+        return PatternGenerator.GeneratePattern(pattern.Axiom, pattern.FMove, pattern.Iterations);
     }
 }
